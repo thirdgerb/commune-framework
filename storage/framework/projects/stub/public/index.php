ce8dbc88b4b84ec98fa511170c5c51e463075389
thirdgerb <thirdgerb@gmail.com>
2016-08-10 16:38:59 +0800

    一些微小的修改,修复了配置中的错误

--- storage/framework/projects/stub/public/index.php
+++ storage/framework/projects/stub/public/index.php
@@ -1,12 +1,5 @@
 <?php
 
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylorotwell@gmail.com>
- */
-
 define('BASE_PATH',realpath(__DIR__.'/../../../'));
 define('PROJECT_PATH',realpath(__DIR__.'/../'));
 
